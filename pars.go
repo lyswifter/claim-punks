24dea6c0f56f256805a2741361d904fc1d78cf9e
lyswifter <ly70835@163.com>
2021-08-31 14:59:34 +0800

    chane tigger timing to 07:20:00

--- pars.go
+++ pars.go
@@ -5,7 +5,7 @@ const repoPath = "~/.claimpunk"
 var wallet_count = 30
 
 // var destTiming = "2021-09-01 20:00:00"
-var destTiming = "2021-08-31 07:02:00"
+var destTiming = "2021-08-31 07:20:00"
 
 var dfxjson = "dfx.json"
 var workDir = "Hell"
